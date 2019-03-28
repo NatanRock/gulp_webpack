a8857e4fbbad51ca7061ff8391a2f7a67352d53d
alex <alx.pedchenko@gmail.com>
2019-03-29 00:22:23 +0200

    add new normalize, add direction function to main.js

--- project/src/assets/scripts/main.js
+++ project/src/assets/scripts/main.js
@@ -57,6 +57,30 @@ function loadAndResize() {
 
 }
 
+
+let lastScrollTop = 0;
+window.addEventListener('scroll', function () {
+    //log('scroll any direction');
+    // enter code here
+
+
+    // scroll direction
+    let st = this.scrollY;
+    if (st > lastScrollTop) {
+        // log('scroll down');
+        // enter code here
+
+    } else if (st == lastScrollTop) {
+        //do nothing
+        //In IE this is an important condition because there seems to be some instances where the last scrollTop is equal to the new one
+    } else {
+        // log('scroll up');
+        // enter code here
+
+    }
+    lastScrollTop = st;
+});
+
 // // check when user out page
 // window.onbeforeunload = function () {
 //     return 'message text';
